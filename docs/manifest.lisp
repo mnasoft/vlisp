b74fe8257144e0b1617acf484b2c7e7155a55193
Nick Matvyeyev <mnasoft@gmail.com>
2019-12-19 19:45:09 +0200

    Чт дек 19 17:45:09 UTC 2019

---
+++ docs/manifest.lisp
@@ -0,0 +1,7 @@
+(:docstring-markup-format :scriba
+ :systems (:vlisp)
+ :documents ((:title "Vlisp"
+              :authors ("Nick Matvyeyev")
+              :output-format (:type :multi-html
+                              :template :gamma)
+              :sources ("manual.scr"))))
